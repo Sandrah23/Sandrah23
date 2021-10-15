c93b95da95d3eaa5acc65ceb23df65c640b18721
Sandrah23 <91742325+Sandrah23@users.noreply.github.com>
2021-10-15 11:43:28 +0300

    Add files via upload

---
+++ NewFile division.cpp
@@ -0,0 +1,12 @@
+/*Arithmetic Operations Division -User friendly Program*/
+#include <stdio.h>
+int main()
+{
+	int a,b, division;
+	printf("In Enter Value to a :");
+	scanf("%d", &a);
+	printf("In Enter Value to b :");
+	scanf("%d", &b);
+	division=a/b;
+	printf("In DIVISION=%d",division);
+}
